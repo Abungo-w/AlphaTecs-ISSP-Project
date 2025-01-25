1f1277535f9822202e5c7152f9b7db53e0d820ba
Peter <hwu138@my.bcit.ca>
2025-01-24 21:24:59 -0800

    added login/register page

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ const path = require("path");
 
 const port = 3000;
 
+app.set('view-engine', 'ejs')
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
+
+app.get('/login',(req, res) => {
+  res.render("login.ejs");
+});
+
+
+app.post('/login', (req, res) => {
+
+})
+
+app.get('/register',(req, res) => {
+  res.render("register.ejs");
+});
+
+app.post('/register', (req, res) => {
+  
+})
 
 app.listen(port, function () {
     console.log(
