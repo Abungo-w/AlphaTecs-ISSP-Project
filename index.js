a71b848feb332a0b04ba8a572bc78dbd76aaf511
Peter <hwu138@my.bcit.ca>
2025-01-25 22:16:55 -0800

    Revert "Merge pull request #1 from bungo-w/peter"
    This reverts commit adf6833d561c813b085132dd6c3afac5a1245d1a, reversing
    changes made to 9eea61a637428dadbf9bf069277677aca7b93aba.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
+
 const app = express();
-const path = require("path");
-
 const port = 3000;
-
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view-engine', 'ejs')
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+    res.render("index.ejs");
 });
 
 app.get('/login',(req, res) => {
@@ -28,10 +25,6 @@ app.get('/register',(req, res) => {
 app.post('/register', (req, res) => {
   
 })
-
-app.get('/upload',(req, res) => {
-  res.render("upload.ejs");
-});
 
 app.listen(port, function () {
   console.log(
