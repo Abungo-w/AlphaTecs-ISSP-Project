8fe9bc4ed9b284cbc032d79f6203795fa16ac86c
Henry Wong <hwong226@my.bcit.ca>
2025-01-29 05:13:04 -0800

    Removed a file in route.

--- routes/profile.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profile_controller');
-
-router.get('/profile', profileController.getProfile);
-router.post('/update-profile', profileController.updateProfile);
-
-module.exports = router;
